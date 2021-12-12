19d09c33c63abfc52d7774ed8e68ff850a0c25d5
ThomasFT3721 <thomas.fontaine.tuffery@gmail.com>
2021-12-12 11:45:02 +0100

    change route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,3 @@
 
 use Zaacom\controllers\ClassGeneratorController;
 use Zaacom\routing\Route;
-
-Route::get(["/Admin/Objects/index"], [ClassGeneratorController::class, "index"], ["name" => "object_generator_index"]);
-Route::get(["/Admin/Objects/generate"], [ClassGeneratorController::class, "generate"], ["name" => "object_generator_generate"]);
