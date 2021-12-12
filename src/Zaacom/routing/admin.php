0ade047dd0318248b1e1a563470742cb13caf96b
ThomasFT3721 <thomas.fontaine.tuffery@gmail.com>
2021-12-12 23:28:11 +0100

    update admin route

--- src/Zaacom/routing/admin.php
+++ src/Zaacom/routing/admin.php
@@ -1,8 +1,9 @@
 <?php
 
+use Zaacom\controllers\ClassGeneratorController;
 use Zaacom\controllers\FolderGeneratorController;
 use Zaacom\routing\Route;
 
 Route::get(["/Admin/Folders/generate"], [FolderGeneratorController::class, "generate"], ["name" => "admin_generate_folders"]);
-Route::get(["/Admin/Objects/index"], [FolderGeneratorController::class, "index"], ["name" => "object_generator_index"]);
-Route::get(["/Admin/Objects/generate"], [FolderGeneratorController::class, "generate"], ["name" => "object_generator_generate"]);
+Route::get(["/Admin/Objects/index"], [ClassGeneratorController::class, "index"], ["name" => "object_generator_index"]);
+Route::get(["/Admin/Objects/generate"], [ClassGeneratorController::class, "generate"], ["name" => "object_generator_generate"]);
