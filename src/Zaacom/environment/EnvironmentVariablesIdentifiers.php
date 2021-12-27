2de7a3eb6d2860cfc8cab905f1152bb86159a8cc
ThomasFT3721 <thomas.fontaine.tuffery@gmail.com>
2021-12-27 06:43:08 +0100

    update environment variable for php version to 8.1

--- src/Zaacom/environment/EnvironmentVariablesIdentifiers.php
+++ src/Zaacom/environment/EnvironmentVariablesIdentifiers.php
@@ -2,18 +2,19 @@
 
 namespace Zaacom\environment;
 
-enum EnvironmentVariablesIdentifiers: string {
-    case APP_NAME = "APP_NAME";
-	case VERSION = "VERSION";
+enum EnvironmentVariablesIdentifiers: string
+{
+	case APP_NAME = 'APP_NAME';
+	case VERSION = 'VERSION';
 
-	case DB_CONNECTION = "DB_CONNECTION";
-	case DB_HOST = "DB_HOST";
-	case DB_PORT = "DB_PORT";
-	case DB_DATABASES = "DB_DATABASES";
-	case DB_USERNAME = "DB_USERNAME";
-	case DB_PASSWORD = "DB_PASSWORD";
+	case DB_CONNECTION = 'DB_CONNECTION';
+	case DB_HOST = 'DB_HOST';
+	case DB_PORT = 'DB_PORT';
+	case DB_DATABASES = 'DB_DATABASES';
+	case DB_USERNAME = 'DB_USERNAME';
+	case DB_PASSWORD = 'DB_PASSWORD';
 
-	case MODE_DEBUG = "MODE_DEBUG";
+	case MODE_DEBUG = 'MODE_DEBUG';
 
-	case BASE_URL = "BASE_URL";
+	case BASE_URL = 'BASE_URL';
 }
