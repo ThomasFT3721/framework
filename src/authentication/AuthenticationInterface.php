95ef04570cc1b01a2323918f81b384303416cd39
ThomasFT3721 <thomas.fontaine.tuffery@gmail.com>
2022-03-11 23:18:16 +0100

    update AuthenticationInterface methods

--- src/authentication/AuthenticationInterface.php
+++ src/authentication/AuthenticationInterface.php
@@ -10,11 +10,11 @@ interface AuthenticationInterface
 	public static function login();
 
 	#[Route(path: '/login', name: 'framework_login', method: RouteMethodEnum::GET)]
-	public static function loginPage();
+	public function loginPage();
 
 	#[Route(path: '/logout', name: 'framework_logout', method: RouteMethodEnum::POST)]
-	public static function logoutPage();
+	public function logoutPage();
 
 	#[Route(path: '/register', name: 'framework_register', method: RouteMethodEnum::GET)]
-	public static function registerPage();
+	public function registerPage();
 }
